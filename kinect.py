a5943850eba96ae36cb570b2332c23a4ad1299d0
Python Whiz 256 <mostwonderfulboy@gmail.com>
2019-09-29 16:01:50 -0600

    Actually add str support

--- kinect.py
+++ kinect.py
@@ -2,16 +2,28 @@ import pexpect as pex
 
 class Py2:
     def __init__(self, file):
-        self.p = pex.spawn("python2 kinect_server.py %s" % file.replace(" ", "\ "))
+        self._Py2_p = pex.spawn("python2 kinect_server.py %s" % file.replace(" ", "\ "))
     def __getattr__(self, attr):
-        self.p.sendline("\x01\x02GET %s" % attr)
+        self._Py2_p.sendline("\x01\x02GET %s" % attr)
         result = ""
         while not result.startswith("\x01\x02"):
-            result = self.p.readline().decode()
+            result = self._Py2_p.readline().decode()
         ret = result[2:]
         try:
             return int(ret)
         except:
-            return float(ret)
+            try:
+                return float(ret)
+            except:
+                if ret[0] + ret[-1] in ["''", '""']:
+                    return eval(ret)
+                elif ret == "ERR_WRONG_TYPE":
+                    raise TypeError("Argument is not of type int, float or str")
+                elif ret == "ERR_NO_ATTR":
+                    raise AttributeError("No attribute %s" % attr)
+                elif ret == "ERR_UNKNOWN_INPUT":
+                    raise IOError("The Py2 server was given unknown input")
+                else:
+                    raise IOError("Unknown input marked as Py2's")
 
 import2 = Py2
--- kinect_server.py
+++ kinect_server.py
@@ -3,9 +3,18 @@ exec("import " + sys.argv[1] + " as module")
 while True:
     want = ""
     while not want.startswith("\x01\x02"):
-        want = raw_input()
+        want = raw_input("{Kinect} ")
     want = want[2:]
     if want.startswith("GET "):
-        result = eval("module." + want[4:])
+        try:
+            result = eval("module." + want[4:])
+        except AttributeError:
+            print("\x01\x02ERR_NO_ATTR")
         if type(result) in [int, float]:
             print("\x01\x02" + str(result))
+        elif type(result) == str:
+            print("\x01\x02" + repr(result))
+        else:
+            print("\x01\x02ERR_WRONG_TYPE")
+    else:
+        print("\x01\x02ERR_UNKNOWN_INPUT")
